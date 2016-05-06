74d115297d975895310eb2a6b2f57b4bfa0d6e99
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2016-05-06 10:35:22 -0400

    added MultiGeneProfileProcess.cpp file

---
+++ sources/MultiGeneProfileProcess.cpp
@@ -0,0 +1,42 @@
+
+/********************
+
+PhyloBayes MPI. Copyright 2010-2013 Nicolas Lartillot, Nicolas Rodrigue, Daniel Stubbs, Jacques Richer.
+
+PhyloBayes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+PhyloBayes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details. You should have received a copy of the GNU General Public License
+along with PhyloBayes. If not, see <http://www.gnu.org/licenses/>.
+
+**********************/
+
+
+#include <cassert>
+
+#include "Parallel.h"
+#include "MultiGeneProfileProcess.h"
+
+void MultiGeneProfileProcess::GlobalUpdateSiteProfileSuffStat()	{
+
+	if (GetNprocs() > 1)	{
+		MPI_Status stat;
+		MESSAGE signal = UPDATE_SPROFILE;
+		MPI_Bcast(&signal,1,MPI_INT,0,MPI_COMM_WORLD);
+	}
+	else	{
+		UpdateSiteProfileSuffStat();
+	}
+}
+
+void MultiGeneProfileProcess::SlaveUpdateSiteProfileSuffStat()	{
+
+	for (int gene=0; gene<Ngene; gene++)	{
+		if (genealloc[gene] == myid)	{
+			process[gene]->UpdateSiteProfileSuffStat();
+		}
+	}
+}
+
